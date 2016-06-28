7683876c64ecd8e39af711920710705b26d56a1d
AJereli <drefaz@gmail.com>
2016-06-28 21:45:31 +0300

    Update

---
+++ WB_Server/WB_Server/Client.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <SFML/System.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+class Client{
+private: sf::TcpSocket client_socket;
+public:
+	Client();
+	Client(sf::TcpSocket * _client_socket);
+	sf::TcpSocket* getClietnSocket() { return &client_socket; }
+	~Client();
+};
+
--- WB_Server/WB_Server/main.cpp
+++ WB_Server/WB_Server/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Network.hpp>
+#include "Client.h"
+#include <list>
+#include <vector>
 
 using namespace std;
+using Server = sf::TcpListener;
+
+const unsigned int port = 8000;
+
+
+
+void startListen(Server & server, list <Client> & client_list) {// Запускает слушающий порт и цикл для принятие новых соединений. Принимает listner-socket и список клиентов
+	server.listen(port); // Слушать порт 8000
+
+	while (1) {
+		Client next_client;
+		if (server.accept(*next_client.getClietnSocket()) == sf::Socket::Done) {
+			client_list.push_back(next_client);
+		}
+	}
+}
 
 int main() {
-	int a, cd;
+	Server server;
+	
+	list <Client> client_list;
+	
+
+	
 
 
 	return 0;
