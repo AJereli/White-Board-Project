7f5324d579b30ae24408da711d8751744fd3f0d4
revi4 <revi4@mail.ru>
2016-06-29 22:45:21 +0300

    Абстрактно-теоретическое представление работы "Авторизации"

--- WB_Client/WB_Client/Form1.Designer.cs
+++ WB_Client/WB_Client/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WB_Client
 {
-    partial class Form1
+    partial class Authorization
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,21 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.LoginTextBox = new System.Windows.Forms.RichTextBox();
+            this.PasswordTextBox = new System.Windows.Forms.RichTextBox();
+            this.EnterBatton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(60, 69);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(150, 30);
+            this.LoginTextBox.TabIndex = 0;
+            this.LoginTextBox.Text = "";
+            this.LoginTextBox.TextChanged += new System.EventHandler(this.LoginTextBox_TextChanged);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(60, 126);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(150, 30);
+            this.PasswordTextBox.TabIndex = 1;
+            this.PasswordTextBox.Text = "";
+            this.PasswordTextBox.TextChanged += new System.EventHandler(this.PasswordTextBox_TextChanged);
+            // 
+            // EnterBatton
+            // 
+            this.EnterBatton.Location = new System.Drawing.Point(96, 182);
+            this.EnterBatton.Name = "EnterBatton";
+            this.EnterBatton.Size = new System.Drawing.Size(75, 23);
+            this.EnterBatton.TabIndex = 2;
+            this.EnterBatton.Text = "Войти";
+            this.EnterBatton.UseVisualStyleBackColor = true;
+            this.EnterBatton.Click += new System.EventHandler(this.EnterBatton_Click);
+            // 
+            // Authorization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.EnterBatton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.LoginTextBox);
+            this.Name = "Authorization";
+            this.Text = "Авторизация";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox LoginTextBox;
+        private System.Windows.Forms.RichTextBox PasswordTextBox;
+        private System.Windows.Forms.Button EnterBatton;
     }
 }
 
--- WB_Client/WB_Client/Form1.cs
+++ WB_Client/WB_Client/Form1.cs
@@ -1,25 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WB_Client
 {
-    public partial class Form1 : Form
+    public partial class Authorization : Form
     {
-        public Form1()
+        public Authorization()
         {
             InitializeComponent();
         }
-
+        private string loginForm;
+        private string passwordForm;
         private void Form1_Load(object sender, EventArgs e)
         {
-            //var uno = "DICH";
+            
+        }
+
+        private void LoginTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnterBatton_Click(object sender, EventArgs e)
+        {
+            loginForm = LoginTextBox.Text;
+            passwordForm = PasswordTextBox.Text;
+            User MainUser = new User();
+            string login = MainUser.getLogin();
+            string password = MainUser.getPassword();
+            if (loginForm == login && passwordForm == password)
+            {
+                MessageBox.Show("Заебись!");
+            }
+            else
+            {
+                MessageBox.Show("Пароль или логин введены неправильно!");
+            }
         }
     }
 }
