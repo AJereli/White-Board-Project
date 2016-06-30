bdd91ede1279c954a999789525e3427916fe8425
AJereli <drefaz@gmail.com>
2016-06-30 17:33:07 +0300

    Start Board

--- WB_Server/WB_Server/consts.h
+++ WB_Server/WB_Server/consts.h
@@ -4,7 +4,7 @@ using namespace std;
 
 const char server_ok_code = 0;
 const char authorize_code = 1;
-const char wrong_pass_code = 2;
+const char wrong_pass_code = -2;
 
 
 const char query_board_code = 5;
@@ -17,10 +17,7 @@ const char query_board_code = 5;
 //////// USERS CODE
 
 const char USER_WAIT_CONFIRM = 0;
-const char USER_CONFIRMED = 1;
-
-
-
+const char USER_ONLINE = 1;
 
 
 
