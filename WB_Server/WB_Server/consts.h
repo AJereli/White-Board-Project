ae057206e6b81de7920980d2976968cd192bb59e
AJereli <drefaz@gmail.com>
2016-06-30 16:27:12 +0300

    Consts

--- WB_Server/WB_Server/consts.h
+++ WB_Server/WB_Server/consts.h
@@ -2,13 +2,12 @@
 using namespace std;
 //////// CODE OF SERVER
 
-const char server_ok_kode = 0;
-const char authorize_kode = 1;
-const char wrong_pass_kode = 2;
+const char server_ok_code = 0;
+const char authorize_code = 1;
+const char wrong_pass_code = 2;
 
 
-
-
+const char query_board_code = 5;
 
 
 
