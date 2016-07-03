9adf6704527f27f8fb22ceb30843389374b5ec28
Goncharalisimys <nik.goncharov.work@mail.ru>
2016-07-04 00:14:16 +0300

    CONNECT2

--- WB_Client/WB_Client/Menu.cs
+++ WB_Client/WB_Client/Menu.cs
@@ -18,27 +18,65 @@ namespace WB_Client
         static public byte[] connect_board_code = new byte[1]; //6
         static public byte[] create_board_err_code = new byte[1]; //105
         static public byte[] server_ok_code = new byte[1];
+        static public byte[] connect_board_err_code = new byte[1]; //106
         public Menu()
         {
             server_ok_code[0] = 0;
             query_board_code[0] = 5;
             create_board_err_code[0] = 105;
             connect_board_code[0] = 6;
+            connect_board_err_code[0] = 106;
             InitializeComponent();
         }
         private void Menu_Load(object sender, EventArgs e)//Создаем меню
         {
 
-        }
-        private void loadOfBoard_Click(object sender, EventArgs e)//Создание новой доски. Работает при клике на нее
-        {
-                    
-        }        
+        }     
          private void exitingFromBoard_Click(object sender, EventArgs e) //Кнопка Exit. Работает при клике на нее
          {
              Application.Exit(); //Закрытие приложения
          }
-        //запрос на создание доски 5!!!!! query_board_code = 5
+        public bool loadingServer(int port)
+        {
+            byte[] bytes = new byte[1024];
+
+            IPAddress ipAddr = IPAddress.Parse("127.1.1.1");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+            Socket client = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            /*int loginLength = Login.Text.Length;*/
+
+            client.Connect(ipEndPoint);
+  
+
+            /* client.Send(Encoding.UTF8.GetBytes(loginLength.ToString()));
+             client.Send(Encoding.UTF8.GetBytes(Login.Text)); */
+
+
+            client.Send(connect_board_code);
+
+            client.Receive(bytes);
+
+            if (bytes[0] == server_ok_code[0])
+                return true;
+
+            else if (bytes[0] == connect_board_err_code[0])
+                return false;
+            else
+                return false;
+        }
+        private void loadOfBoard_Click(object sender, EventArgs e)//Подсоединение к  доске(old_girlfriend). Работает при клике на нее
+        {
+            if (loadingServer(port))
+            {
+                Board F2 = new Board();
+                F2.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Невозможно присоедениться!");
+            }
+        }
         public bool chekingServer(int port)
          {
              byte[] bytes = new byte[1024];
@@ -70,7 +108,7 @@ namespace WB_Client
             }
             else
             {
-                MessageBox.Show("Не сегодня!");
+                MessageBox.Show("Нельзя создать доску!");
             }
         }
     }
